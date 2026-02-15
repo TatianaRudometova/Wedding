4befc2c557b8e70d767643c298c93cb68520b015
TatianaRudometova <155834928+TatianaRudometova@users.noreply.github.com>
2026-02-15 22:42:04 +0500

    Delete gulpfile.js

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const html = require('./gulp-scripts/html');
-const styles = require('./gulp-scripts/styles');
-const scripts = require('./gulp-scripts/scripts');
-const fonts = require('./gulp-scripts/fonts');
-const images = require('./gulp-scripts/images');
-
-gulp.task('build', gulp.parallel(html, styles, scripts, fonts, images));
